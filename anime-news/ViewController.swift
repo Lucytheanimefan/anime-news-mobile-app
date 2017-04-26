2f3348d26c944fa2ddcd0a36a45aa3e4bba28846
Lucy Zhang <lucyzhang@lucys-macbook-pro-2.local>
2017-04-25 23:26:29 -0400

    Initial Commit

---
+++ anime-news/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  anime-news
+//
+//  Created by Lucy Zhang on 4/25/17.
+//  Copyright © 2017 Lucy Zhang. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
