93b151100c62052b1da9924241fa99f7b4b73bf6
Lucy Zhang <spothorse9.lucy@gmail.com>
2017-04-26 03:44:00 -0400

    Table view cell appears

--- anime-news/NewsTableController.swift
+++ anime-news/NewsTableController.swift
@@ -29,23 +29,23 @@ class NewsTableController: UITableViewController {
 
     override func numberOfSections(in tableView: UITableView) -> Int {
         // #warning Incomplete implementation, return the number of sections
-        return 0
+        return 1
     }
 
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         // #warning Incomplete implementation, return the number of rows
-        return 0
+        return 5
     }
 
-    /*
+
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "reuseIdentifier", for: indexPath)
-
+        let cell = tableView.dequeueReusableCell(withIdentifier: "newsId", for: indexPath)
+       
         // Configure the cell...
-
+        cell.textLabel?.text = "Some news \(indexPath.row)"
         return cell
     }
-    */
+
 
     /*
     // Override to support conditional editing of the table view.
