304c2870efad679737df62802b1c557c8b21f414
Lucy Zhang <spothorse9.lucy@gmail.com>
2017-05-03 22:35:34 -0400

    Successful get request

--- anime-news/NewsTableController.swift
+++ anime-news/NewsTableController.swift
@@ -8,91 +8,99 @@
 
 import UIKit
 
+
 class NewsTableController: UITableViewController {
     var anime: [String] = ["Kimi no na wa", "Attack on Titan"]
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        processArticles()
+        //make get request
+        
+        
         // Uncomment the following line to preserve selection between presentations
         // self.clearsSelectionOnViewWillAppear = false
-
+        
         // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
+    
     // MARK: - Table view data source
-
+    
     override func numberOfSections(in tableView: UITableView) -> Int {
         // #warning Incomplete implementation, return the number of sections
         return 1
     }
-
+    
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         // #warning Incomplete implementation, return the number of rows
         return 5
     }
-
-
+    
+    
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "newsId", for: indexPath)
-       
+        
         // Configure the cell...
         cell.textLabel?.text = "Some news \(indexPath.row)"
         return cell
     }
-
+    
+    func processArticles(){
+        makeHTTPRequest(type: "GET", path: "https://lucys-anime-server.herokuapp.com/getNews", body: nil)
+    }
+    
     func add(){
         print("Added something!!!")
     }
     
     // MARK: - UITableViewDelegate Methods
     func tableView(tableView: UITableView, didSelectRowAtIndexPath indexPath: NSIndexPath) {
-       
+        
     }
-
+    
     /*
-    // Override to support conditional editing of the table view.
-    override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the specified item to be editable.
-        return true
-    }
-    */
-
+     // Override to support conditional editing of the table view.
+     override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
+     // Return false if you do not want the specified item to be editable.
+     return true
+     }
+     */
+    
     /*
-    // Override to support editing the table view.
-    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCellEditingStyle, forRowAt indexPath: IndexPath) {
-        if editingStyle == .delete {
-            // Delete the row from the data source
-            tableView.deleteRows(at: [indexPath], with: .fade)
-        } else if editingStyle == .insert {
-            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
-        }    
-    }
-    */
-
+     // Override to support editing the table view.
+     override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCellEditingStyle, forRowAt indexPath: IndexPath) {
+     if editingStyle == .delete {
+     // Delete the row from the data source
+     tableView.deleteRows(at: [indexPath], with: .fade)
+     } else if editingStyle == .insert {
+     // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
+     }
+     }
+     */
+    
     /*
-    // Override to support rearranging the table view.
-    override func tableView(_ tableView: UITableView, moveRowAt fromIndexPath: IndexPath, to: IndexPath) {
-
-    }
-    */
-
+     // Override to support rearranging the table view.
+     override func tableView(_ tableView: UITableView, moveRowAt fromIndexPath: IndexPath, to: IndexPath) {
+     
+     }
+     */
+    
     /*
-    // Override to support conditional rearranging of the table view.
-    override func tableView(_ tableView: UITableView, canMoveRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the item to be re-orderable.
-        return true
-    }
-    */
-
-
+     // Override to support conditional rearranging of the table view.
+     override func tableView(_ tableView: UITableView, canMoveRowAt indexPath: IndexPath) -> Bool {
+     // Return false if you do not want the item to be re-orderable.
+     return true
+     }
+     */
+    
+    
     // MARK: - Navigation
-
+    
     // In a storyboard-based application, you will often want to do a little preparation before navigation
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
         var viewController = segue.destination as! InfoViewController
@@ -103,6 +111,6 @@ class NewsTableController: UITableViewController {
         }
         
     }
-
-
+    
+    
 }
--- anime-news/Utilities.swift
+++ anime-news/Utilities.swift
@@ -9,22 +9,22 @@
 import Foundation
 
 
-
-
 //post request
-func makeHTTPPostRequest(path: String, body: [String: Any]) {
+func makeHTTPRequest(type: String, path: String, body: [String: Any]?) {
     print("Make POST request")
     let request = NSMutableURLRequest(url: NSURL(string: path)! as URL)
     
     // Set the method to POST
-    request.httpMethod = "POST"
+    request.httpMethod = type
     
     do {
         // Set the POST body for the request
         //let jsonBody = try NSJSONSerialization.dataWithJSONObject(body, options: .PrettyPrinted)
         request.addValue("application/json", forHTTPHeaderField: "Content-Type")
         request.addValue("application/json", forHTTPHeaderField: "Accept")
-        request.httpBody = try! JSONSerialization.data(withJSONObject: body, options: [])
+        if let body = body{
+            request.httpBody = try! JSONSerialization.data(withJSONObject: body, options: [])
+        }
         //request.HTTPBody = jsonBody
         let session = URLSession.shared
         
@@ -33,11 +33,9 @@ func makeHTTPPostRequest(path: String, body: [String: Any]) {
                 //let json:JSON = JSON(data: jsonData)
                 //onCompletion(json, nil)
                 print("The Response: ")
-                //print(json)
+                print(data!)
             } else {
                 //onCompletion(nil, error)
-                print("The Response: ")
-                print("Hello")
             }
         })
         task.resume()
