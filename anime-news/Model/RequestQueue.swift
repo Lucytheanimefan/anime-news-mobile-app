d15c56a441e0fc024936491be76c1189653b5af7
Lucy Zhang <spothorse9.lucy@gmail.com>
2017-12-20 22:36:17 +0900

    background check for unsent requests

--- anime-news/Model/RequestQueue.swift
+++ anime-news/Model/RequestQueue.swift
@@ -8,9 +8,17 @@
 
 import UIKit
 import os.log
+import Foundation
+
+protocol RequestQueueDelegate {
+    func makeRequest(body:[String:Any])
+}
+
 class RequestQueue: NSObject {
     
     static let shared = RequestQueue()
+    
+    var delegate:RequestQueueDelegate!
     
     private var _animeQueue:[[String:Any]]! = [[String:Any]]()
     var animeQueue:[[String:Any]]! {
@@ -41,15 +49,43 @@ class RequestQueue: NSObject {
         self.animeQueue.append(request.dict)
     }
     
-    func nextRequest() -> Anime{
-        return Anime(params: self.animeQueue.removeFirst())
-    }
-    
     func removeStaleAnime(anime_id:String){
         self.animeQueue = self.animeQueue.filter({ (animeDict) -> Bool in
             return (animeDict["anime_id"] as? String != anime_id)
         })
         os_log("%@: filtered queue: %@", self.description, self.animeQueue.debugDescription)
+    }
+    
+    private func nextRequest() -> Anime{
+        return Anime(params: self.animeQueue.removeFirst())
+    }
+    
+    private func makeRequest(){
+        let body = nextRequest().dict
+        delegate.makeRequest(body: body)
+    }
+    
+    func scheduleRequests(){
+        // Create an empty XPC dictionary
+        var criteria = xpc_dictionary_create(nil, nil, 0)
+        
+        // Tell XPC that this is a non-repeating activity
+        xpc_dictionary_set_bool(criteria, XPC_ACTIVITY_REPEATING, false)
+        
+        // The activity should start in 1 hour (3600 seconds)
+        xpc_dictionary_set_int64(criteria, XPC_ACTIVITY_DELAY, XPC_ACTIVITY_INTERVAL_1_HOUR)
+        
+        // Allow XPC to defer the activity by as much as 12 hours
+        xpc_dictionary_set_int64(criteria, XPC_ACTIVITY_GRACE_PERIOD, XPC_ACTIVITY_INTERVAL_1_HOUR)
+        
+        // Indicate that this is a user-invisible activity
+        xpc_dictionary_set_string(criteria, XPC_ACTIVITY_PRIORITY, XPC_ACTIVITY_PRIORITY_MAINTENANCE)
+        
+        // Register the new XPC dictionary and pass it the handler block that performs the activity
+        xpc_activity_register("com.anime-news.makeRequest", criteria) { (activity)
+            makeRequest()
+        })
+        
     }
 
 }
