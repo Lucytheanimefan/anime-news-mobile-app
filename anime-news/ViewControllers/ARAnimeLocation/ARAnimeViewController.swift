bc3c7002fd33ffc26785333536dfaba3bb61aa3b
Lucy Zhang <spothorse9.lucy@gmail.com>
2018-01-03 17:29:46 -0500

    Added AR view controller for anime location marking

---
+++ anime-news/ViewControllers/ARAnimeLocation/ARAnimeViewController.swift
@@ -0,0 +1,110 @@
+//
+//  ARAnimeViewController.swift
+//  anime-news
+//
+//  Created by Lucy Zhang on 1/3/18.
+//  Copyright © 2018 Lucy Zhang. All rights reserved.
+//
+
+import UIKit
+import ARKit
+
+class ARAnimeViewController: ARViewController{
+
+    var anchors = [ARAnchor]()
+    // set isPlaneSelected to true when user taps on the anchor plane to select.
+    var isPlaneSelected = false
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    func selectExistingPlane(location: CGPoint) {
+        // Hit test result from intersecting with an existing plane anchor, taking into account the plane’s extent.
+        let hitResults = sceneView.hitTest(location, types: .existingPlaneUsingExtent)
+        if hitResults.count > 0 {
+            let result: ARHitTestResult = hitResults.first!
+            if let planeAnchor = result.anchor as? ARPlaneAnchor {
+                for anchor in anchors {
+                    if anchor.identifier != planeAnchor.identifier{
+                        sceneView.node(for: anchor)?.removeFromParentNode()
+                        sceneView.session.remove(anchor: anchor)
+                    }
+                }
+                
+                // keep track of selected anchor only
+                anchors = [planeAnchor]
+                // set isPlaneSelected to true
+                isPlaneSelected = true
+                //setPlaneTexture(node: sceneView.node(for: planeAnchor)!, imageFilePath: "Hardwood")
+            }
+        }
+    }
+    
+    // checks if anchors are already created. If created, clones the node and adds it the anchor at the specified location
+    func addNodeAtLocation(location: CGPoint) {
+        guard anchors.count > 0 else {
+            print("anchors are not created yet")
+            return
+        }
+        
+        let hitResults = sceneView.hitTest(location, types: .existingPlaneUsingExtent)
+        if hitResults.count > 0 {
+            let result: ARHitTestResult = hitResults.first!
+            let newLocation = SCNVector3Make(result.worldTransform.columns.3.x, result.worldTransform.columns.3.y, result.worldTransform.columns.3.z)
+            let newNode = loadNodeObject(fileName: "ReachPeng", name: "Peng")
+            
+            
+            newNode.position = newLocation
+            
+            // Deal with offset since centered
+            let height = newNode.boundingBox.max.y - newNode.boundingBox.min.y
+            newNode.position.y += height/2
+            sceneView.scene.rootNode.addChildNode(newNode)
+            
+            
+        }
+    }
+
+}
+
+extension ARAnimeViewController: ARSCNViewDelegate{
+    func renderer(_ renderer: SCNSceneRenderer, didAdd node: SCNNode, for anchor: ARAnchor) {
+        if let arPlaneAnchor = anchor as? ARPlaneAnchor{
+            let plane = VirtualPlane(anchor: arPlaneAnchor)
+            plane.setPlaneMaterial(imageName: "Hardwood")
+            self.planes[arPlaneAnchor.identifier] = plane
+            node.addChildNode(plane)
+        }
+    }
+    
+    func renderer(_ renderer: SCNSceneRenderer, didUpdate node: SCNNode, for anchor: ARAnchor) {
+        if let arPlaneAnchor = anchor as? ARPlaneAnchor, let plane = self.planes[arPlaneAnchor.identifier]{
+            plane.updateWithNewAnchor(arPlaneAnchor)
+        }
+    }
+    
+    func renderer(_ renderer: SCNSceneRenderer, didRemove node: SCNNode, for anchor: ARAnchor) {
+        if let arPlaneAnchor = anchor as? ARPlaneAnchor, let index = planes.index(forKey: arPlaneAnchor.identifier){
+            planes.remove(at: index)
+        }
+    }
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        let touch = touches.first!
+        let location = touch.location(in: sceneView)
+        if !isPlaneSelected {
+            selectExistingPlane(location: location)
+        } else {
+            addNodeAtLocation(location: location)
+        }
+    }
+}
---
+++ anime-news/ViewControllers/ARAnimeLocation/ARViewController.swift
@@ -0,0 +1,118 @@
+//
+//  ARViewController.swift
+//  anime-news
+//
+//  Created by Lucy Zhang on 1/2/18.
+//  Copyright © 2018 Lucy Zhang. All rights reserved.
+//
+
+import UIKit
+import ARKit
+
+class ARViewController: UIViewController {
+    @IBOutlet weak var sceneView: ARSCNView!
+    var planes:[UUID: VirtualPlane]! = [UUID: VirtualPlane]()
+    var configuration: ARWorldTrackingConfiguration!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        initializeSceneView()
+        // Do any additional setup after loading the view.
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        self.startSession()
+    }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        sceneView.session.pause()
+    }
+    
+    func initializeSceneView() {
+        // Set the view's delegate
+        sceneView.delegate = self as? ARSCNViewDelegate
+        
+        // Show statistics such as fps and timing information
+        sceneView.showsStatistics = true
+        
+        // Create new scene and attach the scene to the sceneView
+        sceneView.scene = SCNScene()
+        sceneView.autoenablesDefaultLighting = true
+        
+        // Add the SCNDebugOptions options
+        // showConstraints, showLightExtents are SCNDebugOptions
+        // showFeaturePoints and showWorldOrigin are ARSCNDebugOptions
+        //sceneView.debugOptions  = [SCNDebugOptions.showConstraints, SCNDebugOptions.showLightExtents, ARSCNDebugOptions.showFeaturePoints, ARSCNDebugOptions.showWorldOrigin]
+        
+        //shows fps rate
+        sceneView.showsStatistics = true
+        
+        sceneView.automaticallyUpdatesLighting = true
+    }
+    
+    func startSession() {
+        configuration = ARWorldTrackingConfiguration()
+        //currenly only planeDetection available is horizontal.
+        configuration!.planeDetection = ARWorldTrackingConfiguration.PlaneDetection.horizontal
+        sceneView.session.run(configuration!, options: [ARSession.RunOptions.removeExistingAnchors,
+                                                        ARSession.RunOptions.resetTracking])
+        
+    }
+    
+    func setPlaneTexture(node: SCNNode, imageFilePath:String) {
+        if let geometryNode = node.childNodes.first {
+            if node.childNodes.count > 0 {
+                geometryNode.geometry?.firstMaterial?.diffuse.contents = UIImage(named: imageFilePath)
+                geometryNode.geometry?.firstMaterial?.locksAmbientWithDiffuse = true
+                geometryNode.geometry?.firstMaterial?.diffuse.wrapS = SCNWrapMode.repeat
+                geometryNode.geometry?.firstMaterial?.diffuse.wrapT = SCNWrapMode.repeat
+                geometryNode.geometry?.firstMaterial?.diffuse.mipFilter = SCNFilterMode.linear
+            }
+        }
+    }
+    
+    
+    func loadNodeObject(fileName:String, name:String) -> SCNNode
+    {
+        let box = SCNBox(width: 0.001, height: 0.1, length: 0.1, chamferRadius: 0)
+        box.firstMaterial?.diffuse.contents = UIImage(named: fileName)
+        let boxNode = SCNNode()
+        boxNode.geometry = box
+        return boxNode
+    }
+    
+    func stopPlaneDetection(){
+        if let configuration = self.sceneView.session.configuration as? ARWorldTrackingConfiguration
+        {
+            configuration.planeDetection = []
+            self.sceneView.session.run(configuration)
+        }
+    }
+    
+    // want to shine it down so rotate 90deg around the
+    // x-axis to point it down
+    func insertSpotlight(position:SCNVector3){
+        let spotlight = SCNLight()
+        spotlight.type = .spot
+        spotlight.spotInnerAngle = 45
+        spotlight.spotOuterAngle = 45
+        
+        let node = SCNNode()
+        node.light = spotlight
+        node.position = position
+        
+        node.eulerAngles = SCNVector3Make(Float(-Double.pi/2), 0, 0)
+        self.sceneView.scene.rootNode.addChildNode(node)
+    }
+    
+    
+    
+}
+
